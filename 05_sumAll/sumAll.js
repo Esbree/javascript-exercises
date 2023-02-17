ab2fb290ce90f37dd06fd21b1cf5329b4eb08531
Esbree <noelva@gmx.de>
2023-02-18 00:50:07 +0100

    done

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,21 @@
-const sumAll = function() {
+const sumAll = function(a, b) {
+    let sum = 0;
 
+    if (a >= 0 && b >= 0 && typeof a === 'number' && typeof b === 'number') {   // a und b müssen mindestens 0 sein und vom Typ number
+        if (a < b) {
+            for (let i = a; i <= b; i++) {
+                sum = sum + i;
+            }
+        } else {
+            for (let i = b; i <= a; i++) {
+                sum = sum + i;
+            }
+        }
+    } else {
+        return 'ERROR';
+    }
+
+    return sum;
 };
 
 // Do not edit below this line
