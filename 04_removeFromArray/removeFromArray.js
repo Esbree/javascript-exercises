9578733de03aeae462b99211d47f5a35e1061fe5
Esbree <noelva@gmx.de>
2023-02-17 22:06:26 +0100

    removeFromArray 1. done

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,13 @@
-const removeFromArray = function() {
+// 1.
+/* const removeFromArray = function(arr, index) {
+    
+    arr.splice(index -1, 1);
+    return arr;
+    
+}; */
 
-};
+
+
 
 // Do not edit below this line
 module.exports = removeFromArray;
