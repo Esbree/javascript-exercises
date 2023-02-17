8d19fce4171d6d97c28be9bf96dc67824e6d602f
Esbree <noelva@gmx.de>
2023-02-17 23:39:49 +0100

    removeFromArray 2 DONE

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -6,7 +6,12 @@
     
 }; */
 
+// 2. Die Funktion removeFromArray nimmt ein Array array als ersten Parameter und eine variable Anzahl an Argumenten ...args entgegen. Die Funktion gibt ein neues Array zurück, das alle Elemente von array enthält, die nicht in args enthalten sind.
 
+function removeFromArray(array, ...args) {
+    
+    return array.filter(x => !args.includes(x))
+};
 
 // Do not edit below this line
 module.exports = removeFromArray;
