9f65b9933514f1fada170f123482ea34381c0d09
Esbree <noelva@gmx.de>
2023-02-18 01:12:09 +0100

    done

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,16 @@
-const leapYears = function() {
+/* 
+    Schaltjahr:
+        durch 4 teilbar,
+        nicht durch 100 teilbar
+        ODER durch 400 teilbar
+*/
 
+const leapYears = function(year) {
+    if (year % 4 == 0 && year % 100 !== 0 || year % 400 == 0) {
+        return true;
+    } else {
+        return false;
+    }
 };
 
 // Do not edit below this line
