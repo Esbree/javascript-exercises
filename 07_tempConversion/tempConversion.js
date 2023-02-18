8b696850b8f23f55e9f89f6f8ff6264923f6e503
Esbree <noelva@gmx.de>
2023-02-18 10:57:55 +0100

    done

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(deg) {
+  let degCelsius = (deg - 32) * 5 / 9;
+  let rounded = Math.round(degCelsius * 10) / 10  // rundet auf eine Nachkommastelle
+  return rounded;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(deg) {
+  let degFahrenheit = (deg * (9 / 5)) + 32;
+  let rounded = Math.round(degFahrenheit * 10) / 10
+  return rounded;
 };
 
 // Do not edit below this line
