7c4b5cee1e09cad1a8378806c77cea361db4a4f4
Esbree <noelva@gmx.de>
2023-02-24 23:17:52 +0100

    done

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,4 +1,21 @@
-const fibonacci = function() {
+const fibonacci = function(n) {
+
+  let m = n;
+  if (typeof(n) == "string") {
+    m = parseInt(n);
+  }
+
+  if (m < 0) {
+    return 'OOPS';
+  }
+
+  let arr = [0, 1];
+  for (let i = 2; i < m + 1; i++) {
+    arr.push(arr[i - 2] + arr[i -1]);
+  }
+
+  console.log(arr);
+  return arr[m];
 
 };
 
