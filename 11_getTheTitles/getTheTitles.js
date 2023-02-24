94dd1566936fb4802958c73459395596ed92bfb4
Esbree <noelva@gmx.de>
2023-02-24 23:30:54 +0100

    done

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,10 @@
-const getTheTitles = function() {
+const getTheTitles = function(x) {
+  let arr = [];
 
+  arr.push(x[0].title);
+  arr.push(x[1].title);
+
+  return arr;
 };
 
 // Do not edit below this line
