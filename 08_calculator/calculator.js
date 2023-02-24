bd996f635c2c252a41d77951620a218487c54f74
Esbree <noelva@gmx.de>
2023-02-24 19:56:02 +0100

    done

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,44 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+	let sum = array.reduce(function(a, b) {
+    return a + b;
+  }, 0);
+
+  return sum;
 };
 
-const multiply = function() {
+const multiply = function(array) {
+  let sum = 1;
 
+  for (let i = 0; i < array.length; i++) {
+    sum = sum * array[i];
+  }
+  
+  return sum;
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return Math.pow(a, b);
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+	if (num === 0 || num === 1) {
+    return 1;
+  }
+  
+  let result = 1;
+  for (let i = 2; i <= num; i++) {
+    result *= i;
+  }
+
+  return result;
 };
 
 // Do not edit below this line
