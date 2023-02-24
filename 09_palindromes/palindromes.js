0cca9d0488bdd70b57852a28f8c550c26530e37c
Esbree <noelva@gmx.de>
2023-02-24 22:07:36 +0100

    done

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,21 @@
-const palindromes = function () {
+const palindromes = function (string) {
+  let str = (string.replace(/[^a-zA-Z ]/g, "")).toLowerCase();
+  str = str.replaceAll(" ", "");
+  let left = 0;
+  let right = str.length - 1;
+  let rep = Math.floor(str.length / 2);
+  let x = 0;
 
+  while (x < rep) {
+    if(str.charAt(left) != str.charAt(right)) {
+      return false;
+    }
+    left++;
+    right--;
+    x++;
+  }
+
+  return true;
 };
 
 // Do not edit below this line
